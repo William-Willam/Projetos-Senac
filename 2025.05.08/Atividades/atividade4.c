bc6a59167545629f286eadd32792fa9dfaaa2e4a
William dos Santos Rodrigues <williamdossantosrodrigues94@gmail.com>
2025-06-10 17:44:36 -0300

    atividade4

--- 2025.05.08/Atividades/atividade4.c
+++ 2025.05.08/Atividades/atividade4.c
@@ -8,8 +8,28 @@
 
 #include <stdio.h>
 
-int main(){
-    // variaveis
-    int a= 1, b =3;
-    int aux;
+int main() {
+    // 1. Declarando e atribuindo valores iniciais às variáveis 'a' e 'b'
+    int a = 1; 
+    int b = 3; 
+
+    // 2. Imprimindo os valores ANTES da troca
+    printf("Antes da troca:\n");
+    printf("Valor de 'a': %d\n", a);
+    printf("Valor de 'b': %d\n", b);
+    printf("\n"); 
+
+    // 3. Realizando a troca de valores usando uma variável auxiliar
+    int temp; 
+
+    temp = a; 
+    a = b;    
+    b = temp; 
+
+    // 4. Imprimindo os valores DEPOIS da troca
+    printf("Depois da troca:\n");
+    printf("Valor de 'a': %d\n", a);
+    printf("Valor de 'b': %d\n", b);
+
+    return 0; 
 }
